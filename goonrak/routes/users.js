a788893c436d763aebf84e1f31684faabc405d60
ryanking13 <def6488@gmail.com>
2017-09-11 22:44:36 +0900

    add database connection

--- goonrak/routes/users.js
+++ goonrak/routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var db_config = require('../config/db_config.js');
+var connection = mysql.createConnection(db_config);
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+	connection.query('SELECT * FROM test', function(err, rows, field){
+
+		if(err) console.log(err);
+
+		console.log('db func', rows);
+		res.send(rows);
+	});
+
+  //res.send('respond with a resource');
 });
 
 module.exports = router;
