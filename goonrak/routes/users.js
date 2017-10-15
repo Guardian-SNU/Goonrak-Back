4fc053778f27cae237d8d7175bb6e532fe5591fb
ryanking13 <def6488@gmail.com>
2017-10-15 14:39:51 +0900

    fix user test

--- goonrak/routes/users.js
+++ goonrak/routes/users.js
@@ -10,7 +10,7 @@ var email = require('../general/email_auth.js');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 	// test2
-	email.send_verification_email('1', 'highnoo70@gmail.com');
+	email.send_verification_email('1', 'highnoo70@gmail.com', res);
 	//email.verify_token('1', '519', res);
 });
 
